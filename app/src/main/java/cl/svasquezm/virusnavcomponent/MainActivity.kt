b9b0537fb06ce1334031bd70fce827a838b88457
Sebastian Vasquez Morales <sebastian.vasquez@bci.cl>
2020-11-16 18:28:08 -0300

    Create MainActivity with starting FragmentContainerView

--- app/src/main/java/cl/svasquezm/virusnavcomponent/MainActivity.kt
+++ app/src/main/java/cl/svasquezm/virusnavcomponent/MainActivity.kt
@@ -1,11 +1,29 @@
 package cl.svasquezm.virusnavcomponent
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import cl.svasquezm.virusnavcomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            findNavController(R.id.nav_host_fragment).navigate(
+                when (it.itemId) {
+                    R.id.item_home -> R.id.action_global_virusListFragment
+                    R.id.item_help -> R.id.action_global_noImplementedFragment
+                    else -> throw Exception("No valid destination")
+                }
+            )
+
+            false
+        }
     }
 }
